0bf8840e0a1d45d9b3a01cfc28654ab9dfee22db
togami2864 <togami2864@mercari.com>
2025-01-23 00:53:13 +0900

    feat(analyzer): implement visit methods for JsModule and AnyJsModuleItem to enhance type analysi

--- crates/analyzer/src/lib.rs
+++ crates/analyzer/src/lib.rs
@@ -6,18 +6,26 @@ pub struct TypeAnalyzer {}
 
 impl Visitor for TypeAnalyzer {
     fn visit(&mut self, node: &AnyJsRoot) {
-        todo!()
+        match node {
+            AnyJsRoot::JsModule(node) => self.visit_module(node),
+            node => todo!("{:?}", node),
+        }
     }
 
     fn visit_module(&mut self, node: &JsModule) {
-        todo!()
+        for item in node.items() {
+            self.visit_module_item(&item);
+        }
     }
 
     fn visit_module_item(&mut self, node: &AnyJsModuleItem) {
-        todo!()
+        match node {
+            AnyJsModuleItem::AnyJsStatement(node) => self.visit_statement(node),
+            node => todo!("{:?}", node),
+        }
     }
 
     fn visit_statement(&mut self, node: &AnyJsStatement) {
-        todo!()
+        todo!("{:?}", node);
     }
 }
